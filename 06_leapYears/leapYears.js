c71dd0e0763eae3af361fff0bb36fae6302c5b98
Serdar Özdemir <oezdemir50@gmail.com>
2021-09-23 11:17:40 +0200

    Finished the exercises. I want to refactor it to use less if branches

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,18 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 4 === 0) {
+        if (year % 100 === 0) { // we need to check if the year is also dividable by 100
+            if (year % 400 === 0) { // if it is, it needs to be dividable by 400
+                return true;
+            } else {
+                return false;
+            }
+        } else { //return true because it is not dividable by 100.
+            return true;
+        }
+        //after checking it, it might just be a normal year, so check it with this
+    }
+    return false;
 };
-
+console.log(leapYears(1996));
 // Do not edit below this line
 module.exports = leapYears;
