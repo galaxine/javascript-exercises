5f06b5cd2b64c2abd2fd62bf1f65e2edc4638ed0
Serdar Özdemir <oezdemir50@gmail.com>
2021-09-23 12:26:04 +0200

    Refactored the leapYears algorithm. now it needs less lines

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,18 +1,16 @@
 const leapYears = function(year) {
+    //refactored: less lines needed
     if (year % 4 === 0) {
-        if (year % 100 === 0) { // we need to check if the year is also dividable by 100
-            if (year % 400 === 0) { // if it is, it needs to be dividable by 400
-                return true;
-            } else {
-                return false;
-            }
-        } else { //return true because it is not dividable by 100.
+        if (year % 100 === 0 && !(year % 400 === 0)) {
+            return false;
+        } else {
             return true;
         }
-        //after checking it, it might just be a normal year, so check it with this
+    } else {
+        return false;
     }
-    return false;
 };
+
 console.log(leapYears(1996));
 // Do not edit below this line
 module.exports = leapYears;
